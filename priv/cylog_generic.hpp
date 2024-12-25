689bda6c6f182dfc1b07db907935528235491d59
Muatrack <fjfhccfkuk@outlook.com>
2024-12-25 14:19:57 +0800

    重写cylog, base on fs. 上一版本是 base on flash

---
+++ priv/cylog_generic.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#if defined(USE_SYSTEM_FREERTOS)
+    #include <unistd.h>
+    #error "dddddddddddd freertos"
+#else
+    /** for linux */
+    #include <stdint.h>
+#endif
+
+namespace cylog_generic {
+
+using namespace std;
+
+class FileHead {
+public:
+    uint16_t headVer;       // 头部结构的版本 0-65535
+    uint32_t maxLen;        // 文件的最大长度(含头部长度)
+    uint32_t offset;        // 文件的写偏移位置
+};
+
+class CYLogFile {
+private:
+    string name;
+    string path;
+    string maxSize;
+    string curSize;
+};
+
+class CYLogGeneric: CYLogFile {
+private:
+    CYLogFile file;
+public:
+    virtual void path_set(string path) = 0;
+};
+
+};
+// namespace cylog;
---
+++ src/cylog.cpp
@@ -0,0 +1,5 @@
+#include "cylog_generic.hpp"
+
+int main(void) {
+    std::cout << "Hi, CYLOG" << std::endl;
+}
