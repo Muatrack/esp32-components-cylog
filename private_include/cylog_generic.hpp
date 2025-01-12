c4c22d3535d966e786a550b1d84a0a94301be0a8
sernola <18510338192@163.com>
2025-01-12 11:53:49 +0800

    更新父类

--- private_include/cylog_generic.hpp
+++ private_include/cylog_generic.hpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <stdbool.h>
 #include <string>
-
 #include "errno.hpp"
 
 #ifdef USE_SYSTEM_LINUX
@@ -13,7 +12,9 @@
     #include <stdint.h>
 #endif
 
-/* 日志文件头类 */
+////////////////////////////////////////////////////
+
+/* 日志文件-头类 */
 class FileHead {
     public:
     uint16_t headVer;       // 头部结构的版本 0-65535
@@ -21,8 +22,13 @@ class FileHead {
     uint32_t offset;        // 文件的写偏移位置
 };
 
+/* 日志文件-内容类*/
+class FileContent {
+};
+
+/////////////////////////////////////////////////////
+
 /** 日志接口类 */
-template<class T_Data>
 class LogIf {
     public:
     /** 
@@ -30,14 +36,39 @@ class LogIf {
      * @param path 日志文件的路径
      * @param pIn  待输入数据的内存地址
     */
-    virtual CL_TYPE_t read(const std::string &path, T_Data &out ) = 0;
+    virtual CL_TYPE_t read(const std::string &path, FileContent &out ) = 0;
     /** 
      * 写日志 
      * @param path 日志文件的路径
      * @param pIn  待输入数据的内存地址
     */
-    virtual CL_TYPE_t write(const std::string &path, const T_Data &in) = 0;
+    virtual CL_TYPE_t write(const std::string &path, const FileContent &in) = 0;
     /* 删除日志文件 */
     virtual CL_TYPE_t remove(const std::string &path) = 0;
 };
 
+/** 日志抽象类 */
+class LogGeneric : public LogIf {
+
+public:
+    LogGeneric(const std::string&dirPath, const std::string&filePrefix,uint8_t fileMaxCount,uint32_t fileMaxLength):
+    mDirPath(dirPath),mFilePrefix(filePrefix),mFileMaxCount(fileMaxCount),mFileMaxLength(fileMaxLength){}
+
+    CL_TYPE_t read(const std::string &path, FileContent &out );
+    CL_TYPE_t write(const std::string &path, const FileContent &in);
+    CL_TYPE_t remove(const std::string &path);
+protected:
+    std::string mDirPath;        // 日志所属类别的目录
+    std::string mFilePrefix;     // 日志文件名称的前缀
+    uint8_t     mFileMaxCount;   // 所属类别日志文件的数量最大值
+    uint32_t    mFileMaxLength;  // 所属类别日志的单文件大小的上限 <字节>
+};
+
+
+/** 日志类别-告警日志 */
+class AlarmLog : public LogGeneric {
+public:
+    AlarmLog(const std::string & dir, const std::string& fPrefix, uint16_t maxFileCount, uint16_t maxFileLen)
+    :LogGeneric(dir,fPrefix,maxFileCount,maxFileLen){};    
+private:
+};
--- src/cylog_generic.cpp
+++ src/cylog_generic.cpp
@@ -2,3 +2,14 @@
 
 using namespace std;
 
+CL_TYPE_t LogGeneric::read(const std::string &path, FileContent &out ){
+    return CL_OK;
+}
+
+CL_TYPE_t LogGeneric::write(const std::string &path, const FileContent &in){
+    return CL_OK;
+}
+
+CL_TYPE_t LogGeneric::remove(const std::string &path){
+    return CL_OK;
+}
