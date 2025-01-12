12716793576ba9804410a09cf80216e2c56e7117
sernola <18510338192@163.com>
2025-01-12 12:21:08 +0800

    update code env

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-ps aux | grep 'sphinx-autobuild' | grep '/usr/bin/python3' | awk '{print $2}' | xargs kill -9
+ps aux | grep 'sphinx-autobuild' | grep -v 'color=auto' | awk '{print $2}' | xargs kill -9
+
+if [ -d .env ]; then
+   echo "Dir .env exists"
+else
+   echo "Dir .env does not exist"
+   python3 -m venv .env
+   source .env/bin/activate
+   pip install sphinx-autobuild sphinx_rtd_theme
+fi
 
 make clean
 make html
-sphinx-autobuild --host 0.0.0.0 source build/html &
+sphinx-autobuild --host 0.0.0.0 --port 8080 source build/html &
