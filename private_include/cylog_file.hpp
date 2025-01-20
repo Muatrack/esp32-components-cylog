4676615e058cce243e3041c18af175c87d24cb32
sernola <18510338192@163.com>
2025-01-20 16:31:11 +0800

    修改 File 定义

--- private_include/cylog_file.hpp
+++ private_include/cylog_file.hpp
@@ -3,26 +3,27 @@
 
 namespace CLFile {
 
-enum class FileVer:uint8_t {
+enum class FileState:uint8_t {
+    FST_EMPTY,      //新文件,空
+    FST_USING,      //文件使用中
+    FST_FULL,       //文件已满
 };
 
-class CYLogFile{
+class FileAbs{
 public:
-    virtual ~CYLogFile() = 0;
+    virtual ~FileAbs() = 0;
 
     virtual void serialize() = 0;
 };
 
 /* 日志文件-头类 */
-class FileHead : public CYLogFile {
+class FileHead : public FileAbs {
 public:
     FileHead() = delete;
     FileHead(FileHead&fh) = delete;
     FileHead& operator=(FileHead&fh) = delete;
 
-    FileHead(uint8_t ver, uint32_t maxFileLen):m_Ver(ver),m_MaxFLen(maxFileLen) {
-        this->m_WOffset = 0;
-    };
+    FileHead(uint8_t ver, uint32_t maxFileLen) : m_FSt(FileState::FST_EMPTY),m_Ver(ver),m_MaxFLen(maxFileLen) {};
 
     /* 对象序列化 */
     void serialize(){
@@ -30,13 +31,14 @@ public:
     };
 
 private:
-    uint8_t  m_Ver;            // 头部结构的版本 0-65535
-    uint32_t m_MaxFLen;        // 文件的最大长度(含头部长度)
-    uint32_t m_WOffset;        // 文件的写偏移位置
+    FileState   m_FSt;            // 日志文件当前状态
+    uint8_t     m_Ver;            // 头部结构的版本 0-65535    
+    uint32_t    m_MaxFLen;        // 文件的最大长度(含头部长度)
+    // uint32_t m_WOffset;        // 文件的写偏移位置
 };
 
 /* 日志文件-item头*/
-class ItemHead : public CYLogFile {
+class ItemHead : public FileAbs {
 public:
     ItemHead() = delete;
     ItemHead(ItemHead&fh) = delete;
