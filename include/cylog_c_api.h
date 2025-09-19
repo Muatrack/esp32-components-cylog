ed6545caf35ace99e814957cfa9b6202d579586a
Muatrack <a@outlook.com>
2025-09-19 09:15:42 +0800

    删除 废弃的代码

--- include/cylog_c_api.h
+++ include/cylog_c_api.h
@@ -6,8 +6,6 @@ extern "C" {
 
 #include <stdint.h>
 
-void cylog_test();
-
 /**
  * 遍历日志后，以回调的方式将日志内容返回。 定义读取日志的回调函数类型
  * @param data: uint8_t[] 数据的字节序列
@@ -21,9 +19,7 @@ typedef int (log_read_cb_t)(uint8_t data[], uint16_t dataLen );
 
 
 /** 测试用，函数入口 */
-
 void test_alarm_log();
-
 
 #ifdef __cplusplus
 }
