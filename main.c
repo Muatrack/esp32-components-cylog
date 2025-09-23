7610211ff9159e843473b2b541dcd69d65e69883
Hsx <a@163.com>
2025-09-23 21:20:37 +0800

    删除 无用代码

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void test_alarm_log();
-// extern void test_dir_del(  );
-
 extern void alarm_log_test();
 extern void cylog_dir_del( char *path );
 
@@ -11,11 +8,6 @@ int main(void) {
     printf("------------------- Hi, CYLOG ----------------------\n");
 
     cylog_dir_del( "/sdb/logroot" );
-
-    // std::cout << "Hi, CYLOG" << std::endl;
-    // std::cout << "-------- dir removed testing ---------" << std::endl;
-    // test_dir_del("excp/b");
-    // test_alarm_log();
 
     /* 初始化日志目录结构 */
     
