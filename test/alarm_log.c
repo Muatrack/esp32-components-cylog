54dfb5f048721ca213ae27db4391bd092f9e826e
Muatrack <a@outlook.com>
2025-09-23 10:27:18 +0800

    修改 测试代码

--- test/alarm_log.c
+++ test/alarm_log.c
@@ -10,7 +10,7 @@ int cylog_alarm_traversal_cb(uint8_t data[], uint16_t dataLen ) {
     if(data==NULL) { goto done; }
 
     cylog_alarm_t *pAlarm = (cylog_alarm_t *)data;
-    printf("got an expected alarm log with create time:%u\n", pAlarm->createTm );
+    printf("got an expected alarm log with create time:%u\n", pAlarm->base.createTm );
 done:
     return 0;
 }
@@ -38,9 +38,10 @@ void alarm_log_test() {
     uint16_t remainCount = 1000;
     uint32_t seq = 1;
     while ( remainCount-->0 ) {
-        alarm.createTm = (uint32_t)time(NULL);
+        alarm.base.createTm = (uint32_t)time(NULL);
         alarm.seq = seq ++;
-        alarm.type = 0xF0;
+        if(remainCount%600==0)  alarm.type = ALARM_T_3;
+        else alarm.type = ALARM_T_2;
         alarm.val = (seq+2)%4096;
 
         cylog_write(CYLOG_T_ALARM, (uint8_t*)&alarm, sizeof(alarm), 10);
